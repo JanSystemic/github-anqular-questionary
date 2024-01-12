0196ec640278d8d028af9d4177ae19d88c6f004f
JanSystemic <just4fan.jl@gmail.com>
2024-01-12 17:02:24 +0000

    Initial commit

---
+++ src/app/fake-db/questionairs.ts
@@ -0,0 +1,609 @@
+export class QuestionsDb {
+  public static questions = {
+    "questionnaire": {
+      "id": 40,
+      "identifier": "ewBzTS",
+      "name": "Private Liability Insurance",
+      "questions": [
+        {
+          "question_type": "multiple-choice",
+          "identifier": "list_12110972",
+          "headline": "What is your product or service idea?",
+          "description": null,
+          "required": false,
+          "multiple": "true",
+          "choices": [
+          
+          ],
+          "jumps": []
+        },
+        {
+          "question_type": "multiple-choice",
+          "identifier": "list_12110974",
+          "headline": "What problem your [product or service] solve?",
+          "description": null,
+          "required": false,
+          "multiple": "true",
+          "choices": [
+            {
+              "label": "haotic thinking process",
+              "value": "haotic thinking process",
+              "selected": false
+            },
+            {
+              "label": "Time consuming thinking process",
+              "value": "Time consuming thinking process",
+              "selected": false
+            },
+            {
+              "label": "What steps I have to do to develop ideas rapidly?",
+              "value": "What steps I have to do to develop ideas rapidly?",
+              "selected": false
+            }
+          ],
+          "jumps": []
+        },
+        {
+          "question_type": "multiple-choice",
+          "identifier": "list_12110962",
+          "headline": "Select customer segment that you want to profile, choose from options or write your own ideas, but select only one now:",
+          "description": null,
+          "required": false,
+          "multiple": "true",
+          "choices": [
+            {
+              "label": "Small teams",
+              "value": "Small teams",
+              "selected": false
+            },
+            {
+              "label": "Innovators",
+              "value": "Innovators",
+              "selected": false
+            },
+            {
+              "label": "Soloenterprener",
+              "value": "Soloenterprener",
+              "selected": false
+            }
+          ],
+          "jumps": []
+        },
+        {
+          "question_type": "multiple-choice",
+          "identifier": "list_112110962",
+          "headline": "Identify customer jobs. Ask what your customer are trying to complete. Map out all of their jobs by writining each one. Prioritize the most important 3 jobs",
+          "description": null,
+          "required": false,
+          "multiple": "true",
+          "choices": [
+            {
+              "label": "improve or build a business",
+              "value": "improve or build a business",
+              "selected": false
+            },
+            {
+              "label": "make things people want and buy",
+              "value": "make things people want and buy",
+              "selected": false
+            },
+            {
+              "label": "find, learn, apply proven business frameworks",
+              "value": "find, learn, apply proven business frameworks",
+              "selected": false
+            },
+            {
+              "label": "reduce risk of failture",
+              "value": "reduce risk of failture",
+              "selected": false
+            },
+            {
+              "label": "validate new business ideas",
+              "value": "validate new business ideas",
+              "selected": false
+            },
+            {
+              "label": "generate new business ideas",
+              "value": "generate new business ideas",
+              "selected": false
+            }
+          ],
+          "jumps": []
+        },
+        {
+          "question_type": "multiple-choice",
+          "identifier": "list_12110963",
+          "headline": "Identify customer pains. What pains do your customers have? Write down as many as you can come up with. Then prioritize 3 most extreme pains:",
+          "description": null,
+          "required": false,
+          "multiple": "true",
+          "choices": [
+            {
+              "label": "lot a time for thinking process",
+              "value": "lot a time for thinking process",
+              "selected": false
+            },
+            {
+              "label": "to much theory",
+              "value": "to much theory",
+              "selected": false
+            },
+            {
+              "label": "lack of time",
+              "value": "lack of time",
+              "selected": false
+            },
+            {
+              "label": "Making things nobody wants",
+              "value": "Making things nobody wants",
+              "selected": false
+            },
+            {
+              "label": "run of money before revenue stream",
+              "value": "run of money before revenue stream",
+              "selected": false
+            },
+            {
+              "label": "wasting time with not worth ideas",
+              "value": "wasting time with not worth ideas",
+              "selected": false
+            }
+          ],
+          "jumps": []
+        },
+        {
+          "question_type": "multiple-choice",
+          "identifier": "list_12110964",
+          "headline": "Identify customer gains. What outcomes and benefits do your customer want to achive? Write down as many gains as you can come up with. Then prioritize 3 most essential gains:",
+          "description": null,
+          "required": false,
+          "multiple": "true",
+          "choices": [
+            {
+              "label": "leads to results",
+              "value": "leads to results",
+              "selected": false
+            },
+            {
+              "label": "applicable ideas",
+              "value": "applicable ideas",
+              "selected": false
+            },
+            {
+              "label": "easy to understand, clear process",
+              "value": "easy to understand, clear process",
+              "selected": false
+            },
+            {
+              "label": "help when stack",
+              "value": "help when stack",
+              "selected": false
+            },
+            {
+              "label": "specific tips to reduce risk",
+              "value": "specific tips to reduce risk",
+              "selected": false
+            }
+          ],
+          "jumps": []
+        },
+        {
+          "question_type": "multiple-choice",
+          "identifier": "list_12110962",
+          "headline": "Who do you want to insure?",
+          "description": null,
+          "required": false,
+          "multiple": "true",
+          "choices": [
+            {
+              "label": "My family with children",
+              "value": "My family with children",
+              "selected": false
+            },
+            {
+              "label": "My family without children",
+              "value": "My family without children",
+              "selected": false
+            },
+            {
+              "label": "Me without a child",
+              "value": "Me without a child",
+              "selected": false
+            },
+            {
+              "label": "Me with a child",
+              "value": "Me with a child",
+              "selected": false
+            },
+            {
+              "label": "Me and my life partner",
+              "value": "Me and my life partner",
+              "selected": false
+            }
+          ],
+          "jumps": []
+        },
+        {
+          "question_type": "multiple-choice",
+          "identifier": "list_12111610",
+          "headline": "Are you a civil servant or work in public service?",
+          "description": null,
+          "required": false,
+          "multiple": "false",
+          "choices": [
+            {
+              "label": "Yes",
+              "value": "Yes",
+              "selected": false
+            },
+            {
+              "label": "No",
+              "value": "No",
+              "selected": false
+            }
+          ],
+          "jumps": []
+        },
+        {
+          "question_type": "multiple-choice",
+          "identifier": "list_12111777",
+          "headline": "Do you want to insure against non-fulfillment of requirements?",
+          "description": null,
+          "required": false,
+          "multiple": "false",
+          "choices": [
+            {
+              "label": "Yes",
+              "value": "Yes",
+              "selected": false
+            },
+            {
+              "label": "No",
+              "value": "No",
+              "selected": false
+            }
+          ],
+          "jumps": []
+        },
+        {
+          "question_type": "multiple-choice",
+          "identifier": "list_12110966",
+          "headline": "How important is insurance against damage to rented property for you?",
+          "description": null,
+          "required": false,
+          "multiple": "false",
+          "choices": [
+            {
+              "label": "Important",
+              "value": "Important",
+              "selected": false
+            },
+            {
+              "label": "Not important",
+              "value": "Not important",
+              "selected": false
+            }
+          ],
+          "jumps": []
+        },
+        {
+          "question_type": "multiple-choice",
+          "identifier": "list_12110967",
+          "headline": "Are you the owner of one or more properties?",
+          "description": null,
+          "required": false,
+          "multiple": "false",
+          "choices": [
+            {
+              "label": "Yes",
+              "value": "Yes",
+              "selected": false
+            },
+            {
+              "label": "No",
+              "value": "No",
+              "selected": false
+            }
+          ],
+          "jumps": []
+        },
+        {
+          "question_type": "multiple-choice",
+          "identifier": "list_12110968",
+          "headline": "Do you own a residential property?",
+          "description": null,
+          "required": false,
+          "multiple": "false",
+          "choices": [
+            {
+              "label": "Yes, in the country",
+              "value": "Yes, in the country",
+              "selected": false
+            },
+            {
+              "label": "Yes, abroad",
+              "value": "Yes, abroad",
+              "selected": false
+            },
+            {
+              "label": "Yes, in the country and abroad",
+              "value": "Yes, in the country and abroad",
+              "selected": false
+            },
+            {
+              "label": "No",
+              "value": "No",
+              "selected": false
+            },
+            {
+              "label": "No",
+              "value": "No",
+              "selected": false
+            }
+          ],
+          "jumps": []
+        },
+        {
+          "question_type": "multiple-choice",
+          "identifier": "list_12111854",
+          "headline": "Do you plan to build or renovate anything soon?",
+          "description": null,
+          "required": false,
+          "multiple": "false",
+          "choices": [
+            {
+              "label": "Yes",
+              "value": "Yes",
+              "selected": false
+            },
+            {
+              "label": "No",
+              "value": "No",
+              "selected": false
+            }
+          ],
+          "jumps": []
+        },
+        {
+          "question_type": "multiple-choice",
+          "identifier": "list_12110972",
+          "headline": "How important is insurance against loss of professional keys for you?",
+          "description": null,
+          "required": false,
+          "multiple": "false",
+          "choices": [
+            {
+              "label": "Important",
+              "value": "Important",
+              "selected": false
+            },
+            {
+              "label": "Not important",
+              "value": "Not important",
+              "selected": false
+            }
+          ],
+          "jumps": []
+        },
+        {
+          "question_type": "multiple-choice",
+          "identifier": "list_13913438",
+          "headline": "Do you want to insure against the loss of personal keys?",
+          "description": null,
+          "required": false,
+          "multiple": "false",
+          "choices": [
+            {
+              "label": "Yes",
+              "value": "Yes",
+              "selected": false
+            },
+            {
+              "label": "No",
+              "value": "No",
+              "selected": false
+            }
+          ],
+          "jumps": []
+        },
+        {
+          "question_type": "multiple-choice",
+          "identifier": "list_12110969",
+          "headline": "Do you want to be insured abroad?",
+          "description": null,
+          "required": false,
+          "multiple": "false",
+          "choices": [
+            {
+              "label": "Yes, up to one month",
+              "value": "Yes, up to one month",
+              "selected": false
+            },
+            {
+              "label": "Yes, several months",
+              "value": "Yes, several months",
+              "selected": false
+            },
+            {
+              "label": "No",
+              "value": "No",
+              "selected": false
+            }
+          ],
+          "jumps": []
+        },
+        {
+          "question_type": "multiple-choice",
+          "identifier": "list_12110970",
+          "headline": "Do you have a sailing boat?",
+          "description": null,
+          "required": false,
+          "multiple": "false",
+          "choices": [
+            {
+              "label": "Up to 4 m²",
+              "value": "Up to 4 m²",
+              "selected": false
+            },
+            {
+              "label": "Up to 10 m²",
+              "value": "Up to 10 m²",
+              "selected": false
+            },
+            {
+              "label": "Up to 15 m²",
+              "value": "Up to 15 m²",
+              "selected": false
+            },
+            {
+              "label": "Up to 25 m²",
+              "value": "Up to 25 m²",
+              "selected": false
+            },
+            {
+              "label": "No",
+              "value": "No",
+              "selected": false
+            }
+          ],
+          "jumps": []
+        },
+        {
+          "question_type": "multiple-choice",
+          "identifier": "list_12110971",
+          "headline": "Do you have a motorboat?",
+          "description": null,
+          "required": false,
+          "multiple": "false",
+          "choices": [
+            {
+              "label": "Up to 5 HP",
+              "value": "Up to 5 HP",
+              "selected": false
+            },
+            {
+              "label": "Up to 10 HP",
+              "value": "Up to 10 HP",
+              "selected": false
+            },
+            {
+              "label": "Up to 15 HP",
+              "value": "Up to 15 HP",
+              "selected": false
+            },
+            {
+              "label": "Unlimited HP",
+              "value": "Unlimited HP",
+              "selected": false
+            },
+            {
+              "label": "No",
+              "value": "No",
+              "selected": false
+            }
+
+            ],
+            "jumps": []
+          },
+          {
+              "question_type": "multiple-choice",
+          "identifier": "list_12110965",
+          "headline": "Would you like to pay a part of the damage in case of an insurance event?",
+          "description": null,
+          "required": false,
+          "multiple": "false",
+          "choices": [
+            {
+              "label": "No",
+              "value": "No",
+              "selected": false
+            },
+            {
+              "label": "Yes, up to 150 euros",
+              "value": "Yes, up to 150 euros",
+              "selected": false
+            }
+          ],
+          "jumps": []
+        },
+        {
+          "question_type": "multiple-choice",
+          "identifier": "list_12111717",
+          "headline": "Do you already have private liability insurance?",
+          "description": null,
+          "required": false,
+          "multiple": "false",
+          "choices": [
+            {
+              "label": "Yes",
+              "value": "Yes",
+              "selected": false
+            },
+            {
+              "label": "No",
+              "value": "No",
+              "selected": false
+            }
+          ],
+          "jumps": []
+        },
+        {
+          "question_type": "multiple-choice",
+          "identifier": "list_12111755",
+          "headline": "How many liability claims have you had in the last 5 years?",
+          "description": null,
+          "required": false,
+          "multiple": "false",
+          "choices": [
+            {
+              "label": "None",
+              "value": "None",
+              "selected": false
+            },
+            {
+              "label": "1",
+              "value": "1",
+              "selected": false
+            },
+            {
+              "label": "2",
+              "value": "2",
+              "selected": false
+            },
+            {
+              "label": "3",
+              "value": "3",
+              "selected": false
+            },
+            {
+              "label": "More than 3",
+              "value": "More than 3",
+              "selected": false
+            }
+          ],
+          "jumps": []
+        },
+        {
+          "question_type": "text",
+          "identifier": "date_22039590",
+          "headline": "What is your preferred start date for private liability insurance?",
+          "description": null,
+          "required": false,
+          "multiline": "false",
+          "jumps": []
+        },
+        {
+          "question_type": "text",
+          "identifier": "textarea_12110979",
+          "headline": "Do you have any additional information or comments for us?",
+          "description": null,
+          "required": false,
+          "multiline": "true",
+          "jumps": []
+        }
+      ],
+      "description": "To create your personalized private liability insurance offer, we need some information from you.",
+      "category_name_hyphenated": "Private-Liability-Insurance"
+    }
+  };
+}
